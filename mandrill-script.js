f4f9dcf0beba3b8fc6e75cadeafdfbb17829746c
jon-shogren <jon.shogren@mailchimp.com>
2023-10-04 11:15:18 -0400

    added fetch to mandrill api

--- mandrill-script.js
+++ mandrill-script.js
@@ -13,4 +13,39 @@ form.addEventListener('submit', function(event) {
     console.log('Email:', email);
     console.log('Message:', message);
 
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "message": {
+    "html": message,
+    "subject": "New Form Request",
+    "from_email": "",
+    "from_name": "Form Request",
+    "to": [
+      {
+        "email": email,
+        "name": "Form Requester",
+        "type": "to"
+      }
+    ],
+    "headers": {
+      "Reply-To": ""
+    }
+  },
+  "key": ""
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://mandrillapp.com/api/1.0/messages/send.json", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 });
